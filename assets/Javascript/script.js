9409413ff0ece62d6a0db9430f83a78bbeee6daa
Hoger Shoresh <hogga1@gmail.com>
2023-02-08 19:16:28 +0000

    added background image and API query

---
+++ assets/Javascript/script.js
@@ -0,0 +1,27 @@
+function reciepe() {
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://edamam-recipe-search.p.rapidapi.com/search?q=courgettes",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "14b0003685mshe3ead7338141716p16fb2cjsnaa89ee428e3f",
+            "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+
+
+
+
+
+
+
+
+
+        
+    });
+};
